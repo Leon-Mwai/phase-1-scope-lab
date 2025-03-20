652c909f0e16079b70e212d4275b4e8e0aac5329
leon mwai <leon.mwai@student.moringaschool.com>
2025-03-20 11:31:38 +0300

    added index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your solution in this file!
+var customerName = "bob";
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+  }
+  function setBestCustomer() {
+    bestCustomer = "not bob"; // Declared without `var`, `let`, or `const`, making it global
+  }
+  function overwriteBestCustomer() {
+    bestCustomer = "maybe bob"; // Changes the global variable
+  }
+  const leastFavoriteCustomer = "Karen";
+  function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "not Karen"; // This will throw an error
+  }
+  
